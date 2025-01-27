1a3b4a37768df83f7e2e6630c81764f6f5e96d83
Santhosh <murali97t@gmail.com>
2025-01-27 18:18:59 +0530

    new update

---
+++ eclipse/Testing1/src/practice1/Swap.java
@@ -0,0 +1,21 @@
+package practice1;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("checking git");
+
+
+   int a=10 , b=20;
+   
+   
+   a=a+b;//30
+   b=a-b;//10
+   a=a-b; //20
+
+System.out.println("a  = " + a);
+System.out.println("b  = " +b);
+	}
+
+}			
