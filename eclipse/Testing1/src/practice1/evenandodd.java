64f2adf451167d47d20f0acf8e4ab5fb7cc53ece
murali <murali97t@gmail.com>
2025-01-28 16:05:48 +0530

    update

---
+++ eclipse/Testing1/src/practice1/evenandodd.java
@@ -0,0 +1,28 @@
+package practice1;
+
+public class evenandodd {
+
+	public static void main (String arg[])
+	{
+		
+	
+
+	for (int i=0; i<=5; i++)
+	{
+		
+		
+		if(i%2==0)
+
+		System.out.println("it is even number  :" + i);
+	
+		else
+				System.out.println("it is odd number  :" + i);
+	
+	}
+	
+		
+	}
+	
+	
+	
+}
